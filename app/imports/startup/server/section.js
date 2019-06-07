1247cd8a8aa168e00f7511df8414065228292ddf
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-06-07 09:56:10 -1000

    restricted forms property in Sections from Students

--- app/imports/startup/server/section.js
+++ app/imports/startup/server/section.js
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import { Roles } from "meteor/alanning:roles";
 import { Sections } from "../../api/section/section.js";
+import { Students } from "../../api/student/student.js";
+import { Instructors } from "../../api/instructor/instructor.js";
 
 /** Initialize the database with a default data document. */
 function addData(data)
@@ -19,13 +21,36 @@ if (Sections.find().count() === 0)
   }
 }
 
+//  if (Meteor.isServer)
+//  {
+  Meteor.publish("Section", function publish()
+  {
+    if (this.userId)
+    {
+      const studentUsername = Meteor.users.findOne(this.userId).username;
+      const studentSectionNumber = Students.find({ studentName: studentUsername }).sectionNumber;
+      return Sections.find(
+          { roster: { $elemMatch: { studentName: studentUsername } } },
+          { sectionNumber: studentSectionNumber },
+          { fields: { forms: 0 } },
+      );
+    }
+    return this.ready();
+  });
+//  }
+
 /** This subscription publishes only the document from Sections that the logged in admin teaches. */
 Meteor.publish("SectionAdmin", function publish()
 {
   if (this.userId && Roles.userIsInRole(this.userId, "admin"))
   {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Sections.find({ instructors: { $elemMatch: { instructorName: username } } }, { "instructors.$": 1 });
+    const instructorUsername = Meteor.users.findOne(this.userId).username;
+    const instructorSectionNumber = Instructors.find({ instructorName: instructorUsername }).sectionNumber;
+    return Sections.find(
+        { instructors: { $elemMatch: { instructorName: instructorUsername } } },
+        { "instructors.$": 1 },
+        { sectionNumber: instructorSectionNumber },
+    );
   }
   return this.ready();
 });
