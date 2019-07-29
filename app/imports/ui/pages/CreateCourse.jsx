bfa6aaa4389e2df010db489417121b091074131a
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-07-29 11:19:24 -1000

    updated

--- app/imports/ui/pages/CreateCourse.jsx
+++ app/imports/ui/pages/CreateCourse.jsx
@@ -39,67 +39,67 @@ export default class CreateCourseAlternate extends React.Component
             </Grid.Row>
           </Grid>
           <label className="create-course-form-headers">Add students</label>
-          <div>
-            <Table style={{ position: "absolute", width: "1110px", right: "17%", marginTop: "40px" }} definition celled>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell />
-                  <Table.HeaderCell><Icon name="triangle down" />Name</Table.HeaderCell>
-                  <Table.HeaderCell width="4"><Icon name="triangle down" />Student ID</Table.HeaderCell>
-                  <Table.HeaderCell width="3"><Icon name="triangle down" />Section #</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                <Table.Row>
-                  <Table.Cell collapsing>
-                    <Button>Delete</Button>
-                  </Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="Dave Alonzo" fluid/></Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="0000000" fluid/></Table.Cell>
-                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell collapsing>
-                    <Button>Delete</Button>
-                  </Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="Gordon Hayman" fluid/></Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="0000001" fluid/></Table.Cell>
-                  <Table.Cell><Form.Select defaultValue="1" options={ sections } fluid/></Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    <Button>Delete</Button>
-                  </Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="Alfred Mourning" fluid/></Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="0000010" fluid/></Table.Cell>
-                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell collapsing>
-                    <Button>Delete</Button>
-                  </Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="Connor Zamon" fluid/></Table.Cell>
-                  <Table.Cell><Form.Input defaultValue="0000011" fluid/></Table.Cell>
-                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
-                </Table.Row>
-              </Table.Body>
-              <Table.Footer>
-                <Table.Row>
-                  <Table.HeaderCell/>
-                  <Table.HeaderCell colSpan="3">
-                    <Button size="large" style={{ backgroundColor: "#FF7E55", color: "white" }}>Add student</Button>
-                    <Menu className="create-course-table" floated="right" pagination borderless>
-                      <Menu.Item as="a"><Icon name="chevron left" /></Menu.Item>
-                      <Menu.Item as="a">1</Menu.Item>
-                      <Menu.Item as="a">2</Menu.Item>
-                      <Menu.Item as="a">3</Menu.Item>
-                      <Menu.Item as="a"><Icon name="chevron right" /></Menu.Item>
-                    </Menu>
-                  </Table.HeaderCell>
-                </Table.Row>
-              </Table.Footer>
-            </Table>
-          </div>
+          <Table style={{ position: "absolute", width: "1110px", right: "17%", marginTop: "40px" }} definition celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell><Icon name="triangle down" />Name</Table.HeaderCell>
+                <Table.HeaderCell width="4"><Icon name="triangle down" />Student ID</Table.HeaderCell>
+                <Table.HeaderCell width="3"><Icon name="triangle down" />Section #</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Button>Delete</Button>
+                </Table.Cell>
+                <Table.Cell><Form.Input defaultValue="Dave Alonzo" fluid/></Table.Cell>
+                <Table.Cell><Form.Input defaultValue="0000000" fluid/></Table.Cell>
+                <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Button>Delete</Button>
+                </Table.Cell>
+                <Table.Cell><Form.Input defaultValue="Gordon Hayman" fluid/></Table.Cell>
+                <Table.Cell><Form.Input defaultValue="0000001" fluid/></Table.Cell>
+                <Table.Cell><Form.Select defaultValue="1" options={ sections } fluid/></Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <Button>Delete</Button>
+                </Table.Cell>
+                <Table.Cell><Form.Input defaultValue="Alfred Mourning" fluid/></Table.Cell>
+                <Table.Cell><Form.Input defaultValue="0000010" fluid/></Table.Cell>
+                <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Button>Delete</Button>
+                </Table.Cell>
+                <Table.Cell><Form.Input defaultValue="Connor Zamon" fluid/></Table.Cell>
+                <Table.Cell><Form.Input defaultValue="0000011" fluid/></Table.Cell>
+                <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+              </Table.Row>
+            </Table.Body>
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell/>
+                <Table.HeaderCell colSpan="3">
+                  <Button size="large" style={{ backgroundColor: "#FF7E55", color: "white" }}>
+                    Add Another Student
+                  </Button>
+                  <Menu className="create-course-table" floated="right" pagination borderless>
+                    <Menu.Item as="a"><Icon name="chevron left" /></Menu.Item>
+                    <Menu.Item as="a">1</Menu.Item>
+                    <Menu.Item as="a">2</Menu.Item>
+                    <Menu.Item as="a">3</Menu.Item>
+                    <Menu.Item as="a"><Icon name="chevron right" /></Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
         </Container>
     );
   }
