cdf7d91c6d07c8b5e1102cfbcde9f75217120267
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-07-29 11:01:02 -1000

    changed create course page design

--- app/imports/ui/pages/CreateCourse.jsx
+++ app/imports/ui/pages/CreateCourse.jsx
@@ -1,113 +1,105 @@
 import React from "react";
-import { Grid, Form, Container, Table, Icon, Menu, Button } from "semantic-ui-react";
+import { Container, Grid, Icon, Menu, Table, Form, Button } from "semantic-ui-react";
 
-export default class CreateCourse extends React.Component
+export default class CreateCourseAlternate extends React.Component
 {
   render()
   {
+    const sections = [
+      { text: "1", value: "1" },
+      { text: "2", value: "2" },
+      { text: "3", value: "3" },
+      { text: "4", value: "4" },
+    ];
+
     return (
-        <Container>
-          <Grid columns={2} centered verticalAlign="middle">
+        <Container style={{ width: "1000px" }}>
+          <Grid columns={2} style={{ marginBottom: "20px" }}>
             <Grid.Row>
-              <Grid.Column> </Grid.Column>
-              <Grid.Column textAlign="middle" style={{ fontSize: "34px", fontWeight: "bold" }}>Preview</Grid.Column>
-            </Grid.Row>
-            <Grid.Row style={{ padding: "35px 0px 35px 0px" }}>
               <Grid.Column>
-                <Form>
-                  <label className="create-course-form-headers">Course name</label>
-                  <Form.Input name="Course name" placeholder="Name"/>
-                </Form>
+                <label className="create-course-form-headers">Course name</label>
               </Grid.Column>
-              <Grid.Column textAlign="center" style={{ fontSize: "24px", fontWeight: "bold" }}>
-                ICS 239: Intro to Cybersecurity
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row style={{ padding: "35px 0px 35px 0px" }}>
-              <Grid.Column>
-                <Form style={{ position: "relative" }}>
-                  <label className="create-course-form-headers">Add sections</label>
-                  <Form.Input name="Add sections" placeholder="Section #"/>
-                  <Button inverted style={{ padding: "0 0 0 0" }} circular icon>
-                    <Icon className="create-course-buttons" name="plus circle" size="big" />
-                  </Button>
-                </Form>
-              </Grid.Column>
-              <Grid.Column>
-                <Grid columns={2} textAlign="center">
-                  <Grid.Row className="create-course-sections">Section 1 (1)</Grid.Row>
-                  <Grid.Row className="create-course-sections">Section 2 (3)</Grid.Row>
-                  <Grid.Row className="create-course-sections">Section 1 (1)</Grid.Row>
-                  <Grid.Row className="create-course-sections">Section 2 (3)</Grid.Row>
-                </Grid>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row style={{ padding: "35px 0px 35px 0px" }}>
-              <Grid.Column>
-                <Form>
-                  <label className="create-course-form-headers">Add students</label>
-                  <Form.Input name="Student" placeholder="Student name" />
-                  <Form.Input name="Student ID" placeholder="Student ID" />
-                  <Form.Input name="Section #" placeholder="Section #" />
-                  <Button inverted style={{ padding: "0 0 0 0" }} circular icon>
-                    <Icon className="create-course-buttons" name="plus circle" size="big" />
-                  </Button>
-                </Form>
-              </Grid.Column>
-              <Grid.Column>
-                <Table celled>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell><Icon name="triangle down" />Name</Table.HeaderCell>
-                      <Table.HeaderCell width="4"><Icon name="triangle down" />ID</Table.HeaderCell>
-                      <Table.HeaderCell width="3"><Icon name="triangle down" />Section #</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    <Table.Row>
-                      <Table.Cell>Dave Alonzo</Table.Cell>
-                      <Table.Cell>0000000</Table.Cell>
-                      <Table.Cell>2</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>Gordon Hayman</Table.Cell>
-                      <Table.Cell>0000001</Table.Cell>
-                      <Table.Cell>1</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>Alfred Mourning</Table.Cell>
-                      <Table.Cell>0000010</Table.Cell>
-                      <Table.Cell>2</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>Connor Zamon</Table.Cell>
-                      <Table.Cell>0000011</Table.Cell>
-                      <Table.Cell>2</Table.Cell>
-                    </Table.Row>
-                  </Table.Body>
-                  <Table.Footer>
-                    <Table.Row>
-                      <Table.HeaderCell colSpan="3">
-                        <Menu className="create-course-table" floated="right" pagination borderless>
-                          <Menu.Item as="a"><Icon name="chevron left" /></Menu.Item>
-                          <Menu.Item as="a">1</Menu.Item>
-                          <Menu.Item as="a">2</Menu.Item>
-                          <Menu.Item as="a">3</Menu.Item>
-                          <Menu.Item as="a"><Icon name="chevron right" /></Menu.Item>
-                        </Menu>
-                      </Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Footer>
-                </Table>
+              <Grid.Column style={{ display: "flex", justifyContent: "space-between" }}>
+                <label className="create-course-form-headers">Add sections</label>
+                <Button inverted style={{ padding: "0 0 0 0" }} circular icon>
+                  <Icon className="create-course-buttons" name="plus circle" size="big" />
+                </Button>
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-              <Grid.Column> </Grid.Column>
-              <Grid.Column style={{ position: "relative" }}>
-                <Button style={{ position: "absolute", right: "37%" }} size="huge">Submit</Button>
+              <Grid.Column>
+                <Form.Input defaultValue="ICS 239: Intro to Cybersecurity" fluid name="Course name"/>
+              </Grid.Column>
+              <Grid.Column style={{ display: "flex", flexDirection: "column" }}>
+                <Form.Input defaultValue="Section 1" style={{ paddingBottom: "15px" }} fluid name="Add sections 1"/>
+                <Form.Input defaultValue="Section 2" style={{ paddingBottom: "15px" }} fluid name="Add sections 2"/>
+                <Form.Input defaultValue="Section 3" style={{ paddingBottom: "15px" }} fluid name="Add sections 2"/>
+                <Form.Input defaultValue="Section 4" style={{ paddingBottom: "15px" }} fluid name="Add sections 2"/>
               </Grid.Column>
             </Grid.Row>
           </Grid>
+          <label className="create-course-form-headers">Add students</label>
+          <div>
+            <Table style={{ position: "absolute", width: "1110px", right: "17%", marginTop: "40px" }} definition celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell />
+                  <Table.HeaderCell><Icon name="triangle down" />Name</Table.HeaderCell>
+                  <Table.HeaderCell width="4"><Icon name="triangle down" />Student ID</Table.HeaderCell>
+                  <Table.HeaderCell width="3"><Icon name="triangle down" />Section #</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Button>Delete</Button>
+                  </Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="Dave Alonzo" fluid/></Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="0000000" fluid/></Table.Cell>
+                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Button>Delete</Button>
+                  </Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="Gordon Hayman" fluid/></Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="0000001" fluid/></Table.Cell>
+                  <Table.Cell><Form.Select defaultValue="1" options={ sections } fluid/></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>
+                    <Button>Delete</Button>
+                  </Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="Alfred Mourning" fluid/></Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="0000010" fluid/></Table.Cell>
+                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Button>Delete</Button>
+                  </Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="Connor Zamon" fluid/></Table.Cell>
+                  <Table.Cell><Form.Input defaultValue="0000011" fluid/></Table.Cell>
+                  <Table.Cell><Form.Select defaultValue="2" options={ sections } fluid/></Table.Cell>
+                </Table.Row>
+              </Table.Body>
+              <Table.Footer>
+                <Table.Row>
+                  <Table.HeaderCell/>
+                  <Table.HeaderCell colSpan="3">
+                    <Button size="large" style={{ backgroundColor: "#FF7E55", color: "white" }}>Add student</Button>
+                    <Menu className="create-course-table" floated="right" pagination borderless>
+                      <Menu.Item as="a"><Icon name="chevron left" /></Menu.Item>
+                      <Menu.Item as="a">1</Menu.Item>
+                      <Menu.Item as="a">2</Menu.Item>
+                      <Menu.Item as="a">3</Menu.Item>
+                      <Menu.Item as="a"><Icon name="chevron right" /></Menu.Item>
+                    </Menu>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          </div>
         </Container>
     );
   }
